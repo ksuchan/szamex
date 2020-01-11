920ddd8ea32a83149f9e711d2ad254ca9be20807
Denis Peszka <denis.peszka@webdesginsolutions.pl>
2020-01-11 13:03:48 +0100

    logout get

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('restaurants', function(){
     return view('restaurant/search');
