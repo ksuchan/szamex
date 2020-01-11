910c546629eb3fd8ab716237742e59123b9bce73
Kamil Suchan <ksuchan@MacBook-Pro-Kamil.local>
2020-01-11 15:53:35 +0100

    Widok listingu restauracji i pojedynczej restauracji

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -1,56 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="section-title">
+            Wybierz restaurację
+        </div>
 
-<section>
-    <table cellpadding="10">
-        <thead>
-            <tr>
-                <th>
-                    Nazwa:
-                </th>
-                <th>
-                    Adres:
-                </th>
-                <th>
-                    Godziny otwarcia
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($restaurants as $restaurant)
-                <tr>
-                    <td>
-                        <a href="{{ route('restaurant.show', ['restaurant' => $restaurant->id]) }}">
-                            {{ $restaurant->name }}
-                        </a>
-                    </td>
-                    <td>
-                        {{ $restaurant->address  }}
-                    </td>
-                    <td>
-                        <table>
-                            @foreach( $restaurant->openingHours as $h )
-                                <tr>
-                                    <td>
-                                        {{ $h->day }}
-                                    </td>
-                                    <td>
-                                        {{ $h->from . ' - ' . $h->to  }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    </td>
-                </tr>
+        <div class="flex-wrapper restaurant-listing">
+            @forelse ($restaurants as $restaurant)
+                <div class="flex-item">
+                    <div class="restaurant-teaser">
+                        <div class="image">
+                            <div class="media">
+                                <img src="https://placekeanu.com/420/237" />
+                            </div>
+                            <div class="rating">4.5</div>
+                            <div class="choose"><a href="{{ route('restaurant.show', ['restaurant' => $restaurant->id]) }}">Wybieram</a></div>
+                        </div>
+                        <div class="content">
+                            <div class="title">{{ $restaurant->name }}</div>
+                            <div class="badges">
+                                <div class="badge pink">Polska</div>
+                                <div class="badge blue">12 km</div>
+                            </div>
+                            <div class="people">
+                                <div class="people-item">
+                                    <div class="media">
+                                        <img src="{{ asset('images/20x20.png') }}" />
+                                    </div>
+                                </div>
+                                <div class="people-item">
+                                    <div class="media">
+                                        <img src="{{ asset('images/20x20.png') }}" />
+                                    </div>
+                                </div>
+                                <div class="people-item">
+                                    <div class="media">
+                                        <img src="{{ asset('images/20x20.png') }}" />
+                                    </div>
+                                </div>
+                                <div class="people-item">
+                                    <div class="media">
+                                        <img src="{{ asset('images/20x20.png') }}" />
+                                    </div>
+                                    <span class="additionall">+2</span>
+                                </div>
+                            </div>
+                            <div class="address">
+                                {{ $restaurant->address  }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @empty
-                <tr>
-                    <td>
-                        <p>Brak restauracji</p>
-                    </td>
-                </tr>
+                <h3>Brak restauracji</h3>
             @endforelse
-        </tbody>
-    </table>
-</section>
-@endsection
+        </div>
+        <div>
+            @endsection
+
+
+
